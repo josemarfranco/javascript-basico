3e5ffed198cd1a68079ec9807bd2c7a37580927a
Josemar Franco <jsf.franco@gmail.com>
2022-09-04 13:53:48 -0300

    atualizacao v1

---
+++ js/lxs.js
@@ -0,0 +1,25 @@
+// Exemplo #1 hello world
+console.log("Hello world!");
+
+
+// Exemplo #2 operador de comparação
+let operador = 7 === '7'
+console.log(operador);
+
+
+// Exemplo #3 estrutura do if
+var mediaJohn = 7
+
+if (mediaJohn >= 7) {
+    console.log("John foi aprovado! Parabéns!");
+} else {
+    console.log("Não foi dessa vez!!!");
+}
+
+// Exemplo #3 loop for
+let x = 10;
+for (let i = 0; x > 0; i++) {
+    console.log("Oie!")
+    x-= 1;  
+}
+
